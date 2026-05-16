442182046ee86184df98bdc7526bb6a4b1c0ad9a
Christopher Miller <git@chrismiller.xyz>
2026-05-16 13:55:41 -0700

    mcp_http: move plugin/* handlers into handlers/plugins.cc
    Extracts 8 plugin tools (list_available, add, get_description,
    set_parameter, set_enabled, remove, reorder, set_position) plus
    all plugin-domain helpers (plugin_index_for_processor,
    wait_for_plugin_index, wait_for_plugin_post_fader, lower_ascii,
    plugin_type_token, parse_plugin_type_token, plugin_status_string,
    plugin_catalog_id, plugin_matches_search,
    append_plugin_catalog_entries_json, append_all_plugin_infos,
    resolve_plugin_info_for_add, variant_type_string,
    plugin_descriptor_json) and the ScopedInstrumentPromptDisable RAII
    helper into ArdourSurface::mcp.
    
    Largest single extract (~1300 LOC). Code motion only.

---
+++ libs/surfaces/mcp_http/handlers/plugins.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2026 Frank Povazanj <frank.povazanj@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _ardour_surface_mcp_http_handlers_plugins_h_
+#define _ardour_surface_mcp_http_handlers_plugins_h_
+
+#include <string>
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace PBD
+{
+class EventLoop;
+}
+
+namespace ARDOUR
+{
+class Session;
+}
+
+namespace ArdourSurface
+{
+namespace mcp
+{
+
+bool dispatch_plugin_tool_call (
+    ARDOUR::Session&                   session,
+    PBD::EventLoop*                    event_loop,
+    const std::string&                 tool_name,
+    const boost::property_tree::ptree& root,
+    const std::string&                 id,
+    std::string&                       response);
+
+} /* namespace mcp */
+} /* namespace ArdourSurface */
+
+#endif /* _ardour_surface_mcp_http_handlers_plugins_h_ */
